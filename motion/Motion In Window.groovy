b5f78f4864cfc7c15a667c1b4ae0b28967b3e588
Amos Yuen <amosyuen@gmail.com>
2021-02-03 22:03:32 -0800

    Add Motion in Window app

---
+++ motion/Motion In Window.groovy
@@ -0,0 +1,146 @@
+/**
+ *  Copyright 2021 Amos Yuen
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language ginning permissions and limitations under the License.
+ *
+ * 1.0.0 [Amos Yuen] Initial Version
+ */
+
+def appVersion() {
+	return "1.0.0"
+}
+
+definition(
+	name: "Motion In Window",
+	namespace: "amosyuen",
+	author: "Amos Yuen",
+	description: "Creates a motion sensor that is active if there are X instances of motion in a window of Y seconds",
+	category: "Convenience",
+	iconUrl: "",
+	iconX2Url: "",
+	iconX3Url: "",
+)
+
+preferences {
+	 page (name: "mainPage")
+} 
+
+def mainPage() {
+	dynamicPage(name: "mainPage", install: true, uninstall: true) {
+		section("<b>Main</b>") {
+			label title: "Assign a name"
+		}
+	  	section ("<b>Options</b>") {
+			input "motionSensors", "capability.motionSensor", title: "Motion Sensors", repeated: true, required: true
+			input "numberOfMotionEvents", "number", title: "Number of motion events within window required to trigger active", min: 1, defaultValue: 2, required: true
+			input "windowSeconds", "number", title: "Window in seconds", min: "1", required: true
+		}
+		section("<b>Debug</b>") {
+			input "debugLog", "bool", title: "Log debug statements", defaultValue: false, submitOnChange: true
+		} 
+		section {
+			paragraph "v${appVersion()}"
+		}
+	}
+}
+
+/**
+ * Life Cycle
+ */
+
+def installed() {
+	init()
+}
+
+def updated() {
+	init()
+}
+
+def init() {
+	log.info("init")
+	
+	createChildIfNotExist()
+	subscribe(motionSensors, "motion.active", motionSensorHandler)
+
+	if (atomicState.eventsPush) {
+		updateEvents()
+	} else {
+		atomicState.eventsPush = []
+		atomicState.eventsPop = []
+	}
+}
+
+def updateEvents(newEvent = null) {
+	def eventsPop = atomicState.eventsPop
+	def eventsPush = atomicState.eventsPush
+	def nowMillis = now()
+	def windowMillis = windowSeconds * 1000
+	if (newEvent) {
+		eventsPush.add(newEvent)
+	}
+	for (def i = eventsPop.size() - 1; i >= 0; i--) {
+		if (nowMillis - eventsPop[i] <= windowMillis) {
+			break
+		}
+		eventsPop.remove(i)
+	}
+	if (eventsPop.size() == 0 && eventsPush.size() > 0) {
+		for (def i = eventsPush.size() - 1; i >= 0; i--) {
+			def millis = eventsPush[i]
+			if (nowMillis - millis <= windowMillis) {
+				eventsPop.add(millis)
+				eventsPush.remove(i)
+			}
+		}
+	}
+	atomicState.eventsPop = eventsPop
+	atomicState.eventsPush = eventsPush
+
+	def value = eventsPop.size() + eventsPush.size() >= numberOfMotionEvents ? "active" : "inactive"
+	def child = getChildDevice(getChildId())
+	child?.sendEvent(name: "motion", value: value, displayed: true)
+}
+
+def motionSensorHandler(evt) {
+	logDebug("motionSensorHandler: deviceId=${evt.getDevice()} ${evt.name}=${evt.value} @ ${evt.time}")
+	//updateEvents(evt.time())
+}
+
+/**
+ * Child
+ */
+
+def getChildId() {
+	return "motion_in_window_${app.id}"
+}
+
+def createChildIfNotExist() {
+	def id = getChildId()
+    def child = getChildDevice(id)
+    if (!child) {
+        log.info("createChildIfNotExist: creating child device")
+        addChildDevice("hubitat", "Generic Component Motion Sensor", id, /*hubId=*/ null, [name: id, label: app.label, completedSetup: true])
+    }
+}
+
+def componentRefresh(componentDevice) {
+	if (!componentDevice) { return }
+	refresh()
+}
+
+/**
+ * Helpers
+ */
+
+def logDebug(msg){
+	if (debugLog == true) {
+		log.debug msg
+	}
+}
---
+++ motion/README.md
@@ -0,0 +1,3 @@
+# Motion in Window README
+
+Simple app that creates a motion sensor which will have motion set to `active` after X instances of motion in a window of Y seconds. The motion sensor will reset to `inactive` once there are less than X instances of motion in the window.

3ca565cb7366254a5a8424d94f69d70db9cb42bd
Amos Yuen <amosyuen@gmail.com>
2021-05-01 01:12:10 -0700

    Support engaged switches in Garage Manager

--- garage/Garage Manager.groovy
+++ garage/Garage Manager.groovy
@@ -21,56 +21,73 @@
  */
 
 def appVersion() {
-	return "1.0.0"
+	return "1.0.1"
 }
 
 definition(
-	name: 'Garage Manager',
-	namespace: 'amosyuen',
-	author: 'Amos Yuen',
-	description: 'Manage a garage door open and close state',
-	iconUrl: 'https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png',
-	iconX2Url: 'https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png'
+	name: "Garage Manager",
+	namespace: "amosyuen",
+	author: "Amos Yuen",
+	description: "Manage a garage door open and close state",
+	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png"
 )
 
 preferences {
-	page(name: 'pageMain')
+	page(name: "pageMain")
 }
 
 def pageMain() {
-	dynamicPage(name: 'pageMain', uninstall: true, install: true) {
+	dynamicPage(name: "pageMain", uninstall: true, install: true) {
 		section("Main") {
-			label title: "Assign a name", required: true
+			label(title: "Assign a name", required: true)
 		}
 		
-		section('Garage Door') {
-			input name: 'doorControl', type: 'capability.doorControl', title: 'Garage Door Control', required: true
+		section("Garage Door") {
+			input("doorControl", "capability.doorControl", title: "Garage Door Control", required: true)
 		}
 
-		section('Sensors') {
-			input name: 'presenceSensors', type: 'capability.presenceSensor', title: 'Presence Sensors', required: false, multiple: true
-			input "motionSensors", "capability.motionSensor", title: "Motion sensors", multiple: true, required: false
+		section("Sensors") {
+			input("presenceSensors", "capability.presenceSensor", title: "Presence Sensors", required: false, multiple: true)
+			input("motionSensors", "capability.motionSensor", title: "Motion sensors",
+					multiple: true, required: false)
 		}
 		
 		section("Auto Open") {
-			input "openModes", "mode", title: "Modes", multiple: true, required: false, defaultValue: ["Away", "Day"]
-			input "doorDebounceSeconds", "number", title: "Seconds since last door change before it can trigger the door to open", required: true, defaultValue: 300, range: "1..*"
-			input "presenceDebounceSeconds", "number", title: "Seconds presence must be not present before it can trigger the door to open", required: true, defaultValue: 600, range: "1..*"
-			input "autoOpenChangeMode", "mode", title: "Change mode to", required: false, defaultValue: "Day"
+			input("openModes", "mode", title: "Modes", multiple: true, required: false,
+					defaultValue: ["Away", "Day"])
+			input("doorDebounceSeconds", "number",
+					title: "Seconds since last door change before it can trigger the door to open",
+					required: true, defaultValue: 300, range: "1..*")
+			input("presenceDebounceSeconds", "number",
+					title: "Seconds presence must be not present before it can trigger the door to open",
+					required: true, defaultValue: 600, range: "1..*")
+			input("autoOpenSetHsmStatus", "enum",
+					title: "Automatically set HSM status when auto open runs",
+					options: ["armAway", "armHome", "armNight", "disarm", "disarmAll", "armAll", "CancelAlerts"],
+					required: false, defaultValue: "disarm")
 		}
 		
 		section("Auto Close") {
-			input "closeSeconds", "number", title: "Timeout in seconds", required: false, defaultValue: 600, range: "1..*"
-			input "closeAwaySeconds", "number", title: "Timeout in seconds when everyone is away", required: false, defaultValue: 60, range: "1..*"
-			input "closeEngagedSeconds", "number", title: "Timeout in seconds when garage is engaged", required: false, defaultValue: 10800, range: "1..*"
+			input("closeSeconds", "number", title: "Timeout in seconds", required: false,
+					defaultValue: 600, range: "1..*")
+			input("closeAwaySeconds", "number", title: "Timeout in seconds when everyone is away",
+					required: false, defaultValue: 120, range: "1..*")
+			input("engagedSwitches", "capability.switch", title: "Engaged if any switches are on",
+					multiple: true, required: false, submitOnChange: true)
+			if (engagedSwitches) {
+				input("closeEngagedSeconds", "number", title: "Timeout in seconds when garage is engaged",
+						required: false, defaultValue: 10800, range: "1..*")
+			}
 		}
 		
 		section("Notifications") {
-			input "notificationDevices", "capability.notification", title: "Notification Devices", multiple: true, required: false
+			input("notificationDevices", "capability.notification", title: "Notification Devices",
+					multiple: true, required: false)
 		}
 		
 		section("Debug") {
-			input "debugLog", "bool", title: "Log debug statements", defaultValue: false, submitOnChange: true
+			input("debugLog", "bool", title: "Log debug statements", defaultValue: false, submitOnChange: true)
 		} 
 	}
 }
@@ -92,39 +109,38 @@ def init() {
 	unsubscribe()
 	unschedule()
 	
-	atomicState.engaged = atomicState.engaged || false
 	// Re-initialize maps to prevent them growing from removed old presence objects
-	def oldLastPresenceChangeTime = atomicState.lastPresenceChangeTime;
 	def lastPresenceValue = [:]
-	def lastPresenceChangeTime = [:]
+	def lastPresenceChangeMillis = [:]
 	
-	def millis = new Date().getTime()
+	def millis = now()
 	presenceSensors.each{
 		def id = it.id.toString() // Maps only use string keys
 		if (it.currentPresence != lastPresenceValue[id]) {
-			lastPresenceChangeTime[id] = millis
+			lastPresenceChangeMillis[id] = millis
 		} else {
-			def lastMillis = lastPresenceChangeTime[id]
-			lastPresenceChangeTime[id] = lastMillis == null ? millis : lastMillis
+			def lastMillis = lastPresenceChangeMillis[id]
+			lastPresenceChangeMillis[id] = lastMillis == null ? millis : lastMillis
 		}
 		lastPresenceValue[id] = it.currentPresence
 		logDebug("init: update last presence ${id}"
 						 + " lastPresenceValue=${lastPresenceValue[id]}" 
-						 + " lastPresenceChangeTime=${lastPresenceChangeTime[id]}")
+						 + " lastPresenceChangeMillis=${lastPresenceChangeMillis[id]}")
 	}
 	atomicState.lastPresenceValue = lastPresenceValue
-	atomicState.lastPresenceChangeTime = lastPresenceChangeTime
+	atomicState.lastPresenceChangeMillis = lastPresenceChangeMillis
 	
-	atomicState.lastMotionTime = 0
+	atomicState.lastMotionMillis = 0
 	
 	if (doorControl.currentDoor != atomicState.lastDoor) {
 		atomicState.lastDoor = doorControl.currentDoor
-		atomicState.lastDoorChangeTime = millis
+		atomicState.lastDoorChangeMillis = millis
 	}
 	
 	subscribe(doorControl, "door", doorControlHandler)	
 	subscribe(presenceSensors, "presence", presenceHandler) 
 	subscribe(motionSensors, "motion", motionSensorHandler)
+	subscribe(engagedSwitches, "switch", engagedSwitchHandler)
 	
 	closeDoorAfterTimeout()
 }
@@ -141,7 +157,6 @@ def doorControlHandler(evt) {
 	switch (evt.value) {
 		case "closed":
 			unschedule()
-			updateEngaged(false)
 			updateLastDoor(evt)
 			break;
 		case "open":
@@ -159,19 +174,14 @@ def doorControlHandler(evt) {
 
 def updateLastDoor(evt) {
 	atomicState.lastDoor = evt.value
-	atomicState.lastDoorChangeTime = evt.date.getTime()
+	atomicState.lastDoorChangeMillis = evt.date.getTime()
 	logInfo("updateLastDoor: lastDoor=${atomicState.lastDoor}"
-					 + " lastDoorChangeTime=${atomicState.lastDoorChangeTime}")
+					 + " lastDoorChangeMillis=${atomicState.lastDoorChangeMillis}")
 }
 
 def openDoorForPresence(evt) {
 	if (doorControl.currentDoor != "closed") {
 		logDebug("openDoorForPresence: Failed condition currentDoor ${doorControl.currentDoor} is not closed")
-		return
-	}
-		
-	if (openModes && !openModes.contains(location.mode)) {
-		logDebug("openDoorForPresence: Failed condition location.mode ${location.mode} is not in openModes ${openModes}")
 		return
 	}
 		
@@ -186,15 +196,15 @@ def openDoorForPresence(evt) {
 		return
 	}
 		
-	def presenceMillisSinceLastChange = evt.date.getTime() - atomicState.lastPresenceChangeTime[id]
-	if (presenceMillisSinceLastChange < presenceDebounceSeconds * 1000) {
-		logDebug("openDoorForPresence: Failed condition presenceMillisSinceLastChange ${presenceTimeSinceLastChangeMillis} < ${presenceDebounceSeconds * 1000}")
+	def millisSinceLastPresenceChange = evt.date.getTime() - atomicState.lastPresenceChangeMillis[id]
+	if (millisSinceLastPresenceChange < presenceDebounceSeconds * 1000) {
+		logDebug("openDoorForPresence: Failed condition millisSinceLastPresenceChange ${presenceTimeSinceLastChangeMillis} < ${presenceDebounceSeconds * 1000}")
 		return
 	}
 		
-	def doorMillisSinceLastChange = evt.date.getTime() - atomicState.lastDoorChangeTime
-	if (doorMillisSinceLastChange < doorDebounceSeconds * 1000) {
-		logDebug("openDoorForPresence: Failed condition doorMillisSinceLastChange ${doorMillisSinceLastChange} < ${doorDebounceSeconds * 1000}")
+	def millisSinceLastDoorChange = evt.date.getTime() - atomicState.lastDoorChangeMillis
+	if (millisSinceLastDoorChange < doorDebounceSeconds * 1000) {
+		logDebug("openDoorForPresence: Failed condition millisSinceLastDoorChange ${millisSinceLastDoorChange} < ${doorDebounceSeconds * 1000}")
 		return
 	}
 		
@@ -202,8 +212,8 @@ def openDoorForPresence(evt) {
 	if (notificationDevices) {
 		notificationDevices.deviceNotification("Auto-opening garage door!")
 	}
-	if (autoOpenChangeMode) {
-		location.setMode(autoOpenChangeMode)
+	if (autoOpenSetHsmStatus) {
+		sendLocationEvent(name: "hsmSetArm", value: autoOpenSetHsmStatus)
 	}
 	openGarage()
 }
@@ -220,28 +230,24 @@ def presenceHandler(evt) {
 		}
 	}
 	if (atomicState.lastPresenceValue[id] != evt.value) {
-		updateAtomicStateMap('lastPresenceValue', id, evt.value)
-		updateAtomicStateMap('lastPresenceChangeTime', id, evt.date.getTime())
+		updateAtomicStateMap("lastPresenceValue", id, evt.value)
+		updateAtomicStateMap("lastPresenceChangeMillis", id, evt.date.getTime())
 		logDebug("presenceHandler: update last presence ${id}"
 						 + " lastPresenceValue=${atomicState.lastPresenceValue[id]}"
-						 + " lastPresenceChangeTime=${atomicState.lastPresenceChangeTime[id]}")
+						 + " lastPresenceChangeMillis=${atomicState.lastPresenceChangeMillis[id]}")
 	}
 }
 
 def motionSensorHandler(evt) {
 	logDebug("motionSensorHandler ${evt.name}: ${evt.value}")
 	if (evt.value == "active") {
-		atomicState.lastMotionTime = evt.date.getTime()
-	closeDoorAfterTimeout()
+		atomicState.lastMotionMillis = evt.date.getTime()
+		closeDoorAfterTimeout()
 	}
 }
 
-def switchHandler(evt) {
-	logDebug("switchHandler: ${evt.name}: ${evt.value}")
-	atomicState.engaged = evt.value == "on"
-	if (evt.value == "on") {
-		openGarage()
-	}
+def engagedSwitchHandler(evt) {
+	logDebug("engagedSwitchHandler ${evt.name}: ${evt.value}")
 	closeDoorAfterTimeout()
 }
 
@@ -263,7 +269,7 @@ def everyoneAway() {
 }
 
 def getTimeoutSeconds() {
-	if (atomicState.engaged) {
+	if (engagedSwitches && engagedSwitches.currentSwitch.contains("open")) {
 		logDebug("getTimeoutSeconds: engaged ${closeEngagedSeconds}")
 		return closeEngagedSeconds
 	}
@@ -276,12 +282,13 @@ def getTimeoutSeconds() {
 }
 
 def closeDoorAfterTimeout() {
+	unschedule(closeDoorAfterTimeout)
 	if (doorControl.currentDoor == "closed") {
 		return 
 	}
 	
 	def timeoutSeconds = getTimeoutSeconds()
-	def waitMillis = Math.max(atomicState.lastDoorChangeTime, atomicState.lastMotionTime) + timeoutSeconds * 1000 - new Date().getTime()
+	def waitMillis = Math.max(atomicState.lastDoorChangeMillis, atomicState.lastMotionMillis) + timeoutSeconds * 1000 - now()
 	logDebug("closeDoorAfterTimeout: wait ${waitMillis} millis")
 	if (waitMillis <= 0) {
 		atomicState.closeDoorTries = 3
@@ -321,7 +328,7 @@ def tryToCloseDoor() {
 // Helpers
 //
 
-// atomicState maps don't get updated if you try to update it directly
+// atomicState maps don"t get updated if you try to update it directly
 def updateAtomicStateMap(name, key, value) {
 	def map = atomicState[name]
 	map[key] = value
